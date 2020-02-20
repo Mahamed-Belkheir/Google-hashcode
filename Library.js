900326c90680329576b452acfceff3ea309eaffe
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-02-20 21:30:14 +0100

    some tweaks

--- Library.js
+++ Library.js
@@ -1,11 +1,20 @@
 class Library {
-  constructor() {
-    this.books = []
+  constructor(id, count, bootup, rate) {
+    this.id = id;
+    this.books = {};
+    this.rate = rate;
+    this.bootup = bootup;
+    this.shared = {};
+    this.score = 0 ;
+    this.weighted = 0;
+    this.booksCount = count;
   }
 
-  add_book = (book) => {
-    this.books.push(book)
+  add_book = (i, book) => {
+    this.books[i] = book
   }
+
+
 }
 
 module.exports = Library
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Book = require("./Book");
 const Library = require("./Library");
 
 const inputs = ["a_example.txt", "b_read_on.txt", "c_incunabula.txt", "d_tough_choices.txt", "e_so_many_books.txt", "e_so_many_books.txt"];
-const libraries = [];
+
 // for (const input in inputs) {
 //   console.log(`Reading ${input}`);
 //   main(input);
@@ -18,20 +18,40 @@ var main = input => {
   num_books = lines[0].split(" ")[0];
   num_libraries = lines[0].split(" ")[1];
   num_days = lines[0].split(" ")[2];
+  var libraries = [];
 
   var scores = lines[1].split(" ");
-  // console.log(scores);
-  for (var i = 3; i < lines.length; i += 2) {
-    var library = new Library();
-    var books = lines[i].split(" ");
-    // console.log(books);
-    for (var j = 0; j < books.length; j++) {
-      var book = new Book(books[j], scores[books[j]]);
-      library.books.push(book);
+  var totalBooks = scores.map((x, i) => {
+    return {
+      id: i,
+      score: x,
+      in: {}
     }
-    libraries.push(library);
-    console.log(library.books);
+  })
+  for (var i = 2; i < lines.length; i += 2) {
+    var lib = lines[i].split(" ");
+    var library = new Library(lib[0], lib[1], lib[2]);
+    library.id = libraries.length
+    libraries.push(library)
+    var libBooks = lines[i+1].split(" ");
+    libBooks.forEach(b => {
+      totalBooks[b].in[libraries.length-1] = library;
+      totalBooks[b].weighted =  totalBooks[b].score / Object.keys(totalBooks[b]).length
+      library.add_book(b, totalBooks[b])
+      library.score += Number(totalBooks[b].score)
+    })
+
   }
+  libraries.forEach(x => {
+    x.books = Object.values(x.books).sort((a, b) => b.weighted - a.weighted)
+    for(var i of x.books) {
+      x.weighted += i.weighted;
+    }
+  })
+
+  libraries.sort((a, b) => b.weighted - a.weighted)
+
+  return libraries;
 };
 
-main(inputs[0]);
+console.log(main(inputs[0]));
